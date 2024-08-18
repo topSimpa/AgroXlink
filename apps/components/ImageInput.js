4c55eaf0875212748a84636cc55d5cb3abe7ca7f
A-simpa <mohammedabdulmujeeb16@gmail.com>
2024-08-18 20:01:03 +0100

    add: added picture pickage in add ads

--- apps/components/ImageInput.js
+++ apps/components/ImageInput.js
@@ -6,21 +6,26 @@ import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import primary from "../config/colors/primaryColor";
 import neutral from "../config/colors/neutralColor";
 
-function ImageInput({ onPress }) {
+function ImageInput({ onPress, show, children, style }) {
   return (
     <TouchableOpacity
-      style={{
-        width: "100%",
-        height: "100%",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "rgba(26, 28, 18, 0.6)",
-      }}
+      style={[
+        {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(26, 28, 18, 0.6)",
+        },
+        style,
+      ]}
       onPress={onPress}
     >
-      <View>
+      {show ? (
         <MaterialCommunityIcons color={neutral.white} name="camera" size={24} />
-      </View>
+      ) : (
+        children
+      )}
     </TouchableOpacity>
   );
 }
